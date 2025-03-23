7bec4dca1aed5251116005a73f9daa23849209f2
luke <heideluk7@gmail.com>
2025-03-22 21:55:25 -0400

    upped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lukhed_markets",
-    version="0.1.2",
+    version="0.1.3",
     description="A collection of broad market analysis functions and api wrappers",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
