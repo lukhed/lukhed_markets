8cebfae4933fb3573d677e5e561a84e1e321535f
lukhed <lukhed.mail@gmail.com>
2026-01-03 10:24:14 -0500

    fred additional markets

--- lukhed_markets/fred.py
+++ lukhed_markets/fred.py
@@ -140,6 +140,73 @@ class FRED(LukhedAuth):
 
         return df
     
+    def get_manufacturing_employees(self, start_date=None, end_date=None, date_format='%Y-%m-%d'):
+        """
+        Get the manufacturing employees series data.
+        https://fred.stlouisfed.org/series/MANEMP
+
+        Manufacturing employment measures the number of wage and salary workers in the manufacturing sector. 
+        This series is a good indicator of the health of the manufacturing industry and overall economic activity.
+
+        Parameters
+        ----------
+        start_date : str, optional
+            Start date for the data in 'YYYY-MM-DD' format, by default None (earliest available)
+        end_date : str, optional
+            End date for the data in 'YYYY-MM-DD' format, by default None (latest available)
+        date_format : str, optional
+            Date format for start_date and end_date, by default '%Y-%m-%d'
+
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame containing manufacturing employment data.
+        """
+
+        start_date, end_date = self._parse_dates(start_date, end_date, date_format)
+        series = self.api.get_series('MANEMP', observation_start=start_date, observation_end=end_date)
+        
+        # Convert to DataFrame
+        df = pd.DataFrame(series, columns=['Manufacturing_Employees'])
+        
+        # Ensure index is datetime and sorted
+        df.index = pd.to_datetime(df.index)
+        df = df.sort_index()
+
+        return df
+    
+    def federal_governemnt_interest_payments_to_row(self, start_date=None, end_date=None, date_format='%Y-%m-%d'):
+        """
+        Get the federal government interest payments to the rest of the world series data.
+        https://fred.stlouisfed.org/series/B093RC1Q027SBEA
+
+        Parameters
+        ----------
+        start_date : str, optional
+            Start date for the data in 'YYYY-MM-DD' format, by default None
+        end_date : str, optional
+            End date for the data in 'YYYY-MM-DD' format, by default None
+        date_format : str, optional
+            Date format for start_date and end_date, by default '%Y-%m-%d'
+
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame containing federal government interest payments to the rest of the world data.
+        """
+
+        start_date, end_date = self._parse_dates(start_date, end_date, date_format)
+        series = self.api.get_series('B093RC1Q027SBEA', observation_start=start_date, observation_end=end_date)
+        
+        # Convert to DataFrame
+        df = pd.DataFrame(series, columns=['Fed_Govt_Interest_Payments_to_ROW'])
+        
+        # Ensure index is datetime and sorted
+        df.index = pd.to_datetime(df.index)
+        df = df.sort_index()
+
+        return df
+    
     def plot_pce_inflation_rate(self, start_date=None, end_date=None, date_format='%Y-%m-%d', 
                                 include_averages=True, show_plot=True, save_plots=False):
         """
@@ -159,7 +226,14 @@ class FRED(LukhedAuth):
         save_plots : bool, optional
             Whether to save the plots as PNG files, by default False
             **Saved plots will be stored in a 'plots' directory.**
+        show_plot : bool, optional
+            Whether to display the plot, by default True
+
+        Returns
+        -------
+        None
         """
+        
         import matplotlib.pyplot as plt
 
         # First substract 1 year from start date to ensure we have data for YoY calculation
